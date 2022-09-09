e8e2d3177332c767bae05c0a70a1bb4c7e298ea2
Luis Garelli <luiscarlosgarelli@gmail.com>
2022-09-09 11:50:22 +0200

    third tests

--- src/test/java/TpPageObject.java
+++ src/test/java/TpPageObject.java
@@ -48,7 +48,7 @@ public class TpPageObject {
 
     Actions actions = new Actions(driver);
     actions.moveToElement(loginButton);
-    actions.perform();  // second test
+    actions.perform();  // third test
 
     try {
       Thread.sleep(4000);
