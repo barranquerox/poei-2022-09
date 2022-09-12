e2455051a96608520f5b00ff3da595c59d0e94ce
Luis Garelli <luiscarlosgarelli@gmail.com>
2022-09-12 15:06:30 +0200

    removed broken tests

--- src/test/java/TP3Test.java
+++ src/test/java/TP3Test.java
@@ -1,2 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class TP3Test {
+
+  @Test
+  public void testSimple() {
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://www.google.com");
+    driver.quit();
+  }
 }
